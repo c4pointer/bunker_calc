b41e9897d505f43db2524dd6d994c7cf4e99d8f3
Oleg Zubak <c4point@gmail.com>
2022-11-19 15:20:14 +0200

    Improvements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import db_reading
 import db_editing
 
-import kivy
+
 
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
@@ -9,12 +9,13 @@ from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.icon_definitions import md_icons
 from kivymd.uix.button import MDFlatButton
-from kivy.lang import Builder
+# from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
-
 import sqlite3
 
-
+# Window resizing. To be deleted before compiling
+from kivy.core.window import Window
+Window.size=(360,600)
 
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
@@ -23,11 +24,11 @@ class Tab(MDFloatLayout, MDTabsBase):
 #     '''Class implementing content for a tab.'''
 
 
-class Store_Values():
+# class Store_Values():
 
-    def __init__(self, sound, tk_name):
-        self.sound = sound
-        self.tk_name = tk_name
+#     def __init__(self, sound, tk_name):
+#         self.sound = sound
+#         self.tk_name = tk_name
 
 
 class BunkerCalc(MDApp):
@@ -35,9 +36,9 @@ class BunkerCalc(MDApp):
     def build(self):
 
         # Theme and colors
-        self.theme_cls.primary_palette = "BlueGray"
-        self.theme_cls.primary_hue = "600"
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Gray"
+        self.theme_cls.primary_hue = "500"
+        self.theme_cls.theme_style = "Dark"
 
 
 
@@ -52,11 +53,6 @@ class BunkerCalc(MDApp):
     def on_start(self):
             self.name_of_tank()
             self.add_tab()
-            
-    # def get_tab_list(self):
-    #     '''Prints a list of tab objects.'''
-
-    #     print(self.root.ids.tabs.get_tab_list())
 
     def add_tab(self):
 
@@ -75,7 +71,8 @@ class BunkerCalc(MDApp):
         '''
 
         instance_tab.ids.sound_field.hint_text = "Sounding value (cm):"
-
+        instance_tab.ids.sound_field.text_color_normal = 1,1,0.8,1
+        
         self.sound_value= instance_tab.ids.sound_field
         self.tank_name = instance_tab_label
         self.result = instance_tab.ids.label
@@ -84,11 +81,12 @@ class BunkerCalc(MDApp):
 
         db_editing.calculation(self.tank_name.text, self.sound_value.text)
         try:
+            self.result.font_size= "60dp"
             self.result.text = str(db_editing.volume_in_m3[0]) + str(" m3")
         except IndexError as e:
-            self.result.text = str(e)
-        # r = (self.root.ids.tabs.get_tab_list())
-        # print(r)
+            # Printing on display the error and change the font-size
+            self.result.text = str("Wrong sounding value!")
+            self.result.font_size= "20dp"
 
 
 if __name__ == "__main__":
