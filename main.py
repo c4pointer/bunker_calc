ec9f15f50e82648ebac381bcde59fb8b927c95fb
Oleg Zubak <c4point@gmail.com>
2022-11-20 03:00:37 +0200

    Screens added

--- main.py
+++ main.py
@@ -16,13 +16,27 @@ import sqlite3
 
 # Window resizing. To be deleted before compiling
 from kivy.core.window import Window
-Window.size=(360,600)
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.tab import MDTabsBase
+
+import db_editing
+import db_reading
+
+Window.size = (360, 600)
+
+total_list={}
 
 class TabScreen(Screen):
     pass
 
+
 class TotalScreen(Screen):
     pass
+
+
+
 
 class Tab(MDFloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
@@ -34,11 +48,12 @@ class Tab(MDFloatLayout, MDTabsBase):
 #         self.sound = sound
 #         self.tk_name = tk_name
 
-sm=ScreenManager()
+sm = ScreenManager()
 sm.add_widget(TabScreen(name='tab_screen'))
 sm.add_widget(TotalScreen(name='total_screen'))
+
+
 class BunkerCalc(MDApp):
-    
 
     def build(self):
 
@@ -48,27 +63,31 @@ class BunkerCalc(MDApp):
         self.theme_cls.theme_style = "Dark"
 
     def dropdown(self, instance):
-        self.menu_items=[
+        self.menu_items = [
             {
-            "viewclass":"OneLineListItem",
-            "text": "Tanks Sounding",
-            "on_release":lambda x="Tanks Sounding":self.screen1()
-        },
-        {
-            "viewclass": "OneLineListItem",
-            "text": "Total Result",
-            "on_release": lambda x="Total Result":self.screen2()
-        }
+                "viewclass": "OneLineListItem",
+                "text": "Tanks Sounding",
+                "on_release": lambda x="Tanks Sounding": self.screen1()
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Total Result",
+                "on_release": lambda x="Total Result": self.screen2()
+            }
         ]
-        self.menu=MDDropdownMenu(
+        self.menu = MDDropdownMenu(
             items=self.menu_items,
             width_mult=4
         )
-        self.menu.caller=instance
+        self.menu.caller = instance
         self.menu.open()
 
     def screen2(self):
+
         self.root.current = "total_screen"
+
+
+        self.root.get_screen("total_screen").ids.total.text=str(total_list)
 
     def screen1(self):
 
@@ -86,11 +105,10 @@ class BunkerCalc(MDApp):
     def add_tab(self):
 
         for i in (self.names):
-            print(i)
             self.root.get_screen('tab_screen').ids.tabs.add_widget(Tab(tab_label_text=f"{i}"))
 
     def on_tab_switch(
-        self, instance_tabs, instance_tab, instance_tab_label, tab_text
+            self, instance_tabs, instance_tab, instance_tab_label, tab_text
     ):
         '''Called when switching tabs.
 
@@ -101,9 +119,9 @@ class BunkerCalc(MDApp):
         '''
 
         instance_tab.ids.sound_field.hint_text = "Sounding value (cm):"
-        instance_tab.ids.sound_field.text_color_normal = 1,1,0.8,1
+        instance_tab.ids.sound_field.text_color_normal = 1, 1, 0.8, 1
 
-        self.sound_value= instance_tab.ids.sound_field
+        self.sound_value = instance_tab.ids.sound_field
         self.tank_name = instance_tab_label
         self.result = instance_tab.ids.label
 
@@ -111,13 +129,16 @@ class BunkerCalc(MDApp):
 
         db_editing.calculation(self.tank_name.text, self.sound_value.text)
         try:
-            self.result.font_size= "60dp"
+            self.result.font_size = "60dp"
+            self.result.text = str(db_editing.volume_in_m3[0])
+            total_list[self.tank_name.text]=(self.result.text)
             self.result.text = str(db_editing.volume_in_m3[0]) + str(" m3")
+
 
         except IndexError as e:
             # Printing on display the error and change the font-size
             self.result.text = str("Wrong sounding value!")
-            self.result.font_size= "20dp"
+            self.result.font_size = "20dp"
 
     def on_start(self):
 
@@ -125,6 +146,6 @@ class BunkerCalc(MDApp):
 
         self.add_tab()
 
+
 if __name__ == "__main__":
     BunkerCalc().run()
-
