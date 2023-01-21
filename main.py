51a8d1148d2f362f6cd8c78d7ef5c04a1a08a54e
Oleg Zubak <c4point@gmail.com>
2023-01-21 21:43:00 +0200

    bug with temp slider fixed

--- main.py
+++ main.py
@@ -146,7 +146,8 @@ class BunkerCalc(MDApp):
                     self.total_result_hfo.append(total_list_hfo[i])
 
             except KeyError as err:
-               print("eror string 147")
+                # print("eror string 147")
+                pass
         
 
 
@@ -163,7 +164,8 @@ class BunkerCalc(MDApp):
                     self.total_result_mdo.append(total_list_mdo[d])
 
             except KeyError as err:
-                print("eror string 164")
+                # print("eror string 164")
+                pass
         
         for i in self.total_result_mdo:
             self.sum_mdo += float(i[4])
@@ -373,18 +375,16 @@ class BunkerCalc(MDApp):
                     if db_editing.type_of_tank[0] == 1:
                         self.result.text=str(self.sound_value.text)
                         volume = self.result.text
-                        main_data =calculation.Calcuation()
-                        main_data.temp_dens_extraction()
+                        self.temp_dens_extraction()
                         # Define below row for take into prev function use
-                        total_list_mdo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] = ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
+                        total_list_mdo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =  ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
                         self.result.text = str(self.sound_value.text) + str(" m3")
 
                     else:
                         self.result.text = str(db_editing.volume_in_m3[0])
-                        main_data =calculation.Calcuation()
-                        main_data.temp_dens_extraction()
+                        self.temp_dens_extraction()
                         # Define below row for take into prev function use
-                        total_list_mdo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] = ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
+                        total_list_mdo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =  ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
                         self.result.text = str(db_editing.volume_in_m3[0]) + str(" m3")
                                 
                 
@@ -416,20 +416,18 @@ class BunkerCalc(MDApp):
                     if db_editing.type_of_tank[0] == 1:
                         self.result.text=str(self.sound_value.text)
                         volume = self.result.text
-                        main_data =calculation.Calcuation()
-                        main_data.temp_dens_extraction()
+                        self.temp_dens_extraction()
                         # Define below row for take into prev function use
-                        total_list_hfo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =   ((main_data.real_volume), self.sound_value.text, main_data.temperature, main_data.def_dens, volume)
+                        total_list_hfo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =   ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
                         self.result.text = str(self.sound_value.text) + str(" m3")
 
                     else:
                         self.result.font_size = "30dp"
                         self.result.text = str(db_editing.volume_in_m3[0])
-                        main_data =calculation.Calcuation()
-                        main_data.temp_dens_extraction() 
+                        self.temp_dens_extraction() 
 
                         # Define below row for take into prev function use
-                        total_list_hfo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =   ((main_data.real_volume), self.sound_value.text, main_data.temperature, main_data.def_dens, volume)
+                        total_list_hfo[str(self.tank_name.text.removesuffix('mdo')).strip(' ')] =   ((self.real_volume), self.sound_value.text, self.temperature, self.def_dens, volume)
                         self.result.text = str(db_editing.volume_in_m3[0]) + str(" m3")
                                 
                 
@@ -452,10 +450,75 @@ class BunkerCalc(MDApp):
             self.root.get_screen("tab_screen").ids.select_vessel.text = "Select the vessel first"
             print(e)
         
-    def my_value(self, value):  # <<<<<<<<<<< Value from Temp slider
-        self.slider_value={}
-        self.slider_value[str(self.tank_name.text.removesuffix('mdo')).strip(' ')]= str(int(round(value,0)))
+    def my_value(self, *args):  # <<<<<<<<<<< Value from Temp slider
+        try:
+            self.slider_value={}
+            self.slider_value[str(self.tank_name.text.removesuffix('mdo')).strip(' ')]= str(float(args[1]))
+        except Exception as e:
+            print(e)
+        return self.slider_value
 
+    def temp_dens_extraction(self):
+            try:
+
+                if len(self.slider_value) !=0 :
+                    self.temperature = str(self.slider_value[str(self.tank_name.text.removesuffix('mdo')).strip(' ')])
+                else:
+                    self.temperature = def_temp
+
+                # Density selecting
+                if len(self.dens_new.text) == 0:
+                    # If density is not inputed by user than we collect it from
+                    # database
+                    self.def_dens = db_editing.select_DefDens(str(self.tank_name.text.removesuffix('mdo')).strip(' '), self.name_of_vessel_db)
+
+                else:
+                    # if is inputed than put that what user inputed
+                    if float(self.dens_new.text) >= 1.1:
+                        try:
+                            self.dens_new.hint_text = "Wrong Density"
+                            self.dens_new.text_color_normal = "#ff2233"
+                        except Exception as e:
+                            print(e)
+                            print("HERE")
+                            
+                    else:
+                        self.dens_new.hint_text = "Density (example: 0.9588)"
+                        self.dens_new.text_color_normal = 1, 1, 0.8, 1
+                        self.def_dens = self.dens_new.text
+                
+
+            except AttributeError as e:
+                self.temperature = def_temp
+                print("eror string 490" + str(self.temperature))
+                if len(self.dens_new.text) == 0:
+                    # If density is not inputed by user than we collect it from
+                    # database
+                    self.def_dens = db_editing.select_DefDens(str(super.tank_name.text.removesuffix('mdo')).strip(' '), super.name_of_vessel_db)
+                else:
+                    # if is inputed than put that what user inputed
+                    if float(self.dens_new.text) >= 1.1:
+                        try:
+                            self.dens_new.hint_text = "Wrong Density"
+                            self.dens_new.text_color_normal = "#ff2233"
+                        except Exception as e:
+                            print(e)
+                            print("eror string 498")
+                    else:
+                        self.dens_new.hint_text = "Density (example: 0.9588)"
+                        self.dens_new.text_color_normal = 1, 1, 0.8, 1
+                        self.def_dens = self.dens_new.text
+
+
+            try:
+                self.converted_density = ((float(self.def_dens)/2)*1000)*2       
+                vol_coorection.vol_correction_factor_calc(self.converted_density, self.result.text, self.temperature)
+                self.real_volume = vol_coorection.result
+            except Exception as e:
+                print(e)
+                print("eror string 511")
+
+            return self.temperature, self.def_dens, self.real_volume
 
 
 if __name__ == "__main__":
